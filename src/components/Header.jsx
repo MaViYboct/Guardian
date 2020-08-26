6f0ab13e3a7d58338d2ba58ca62875308cc87e0b
MaViYboct <kaankaan111@gmail.com>
2020-08-26 04:22:54 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SecurityIcon from "@material-ui/icons/Security";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <SecurityIcon /> Guardian
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
